559c92d4197ebba3782f00aaf32667610d0e3461
JamesNg <njames15@gmail.com>
2022-04-27 10:29:28 -0400

    commit tree b0d3addc5d2d412978865fd36a0de526fbd0da7b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(nameArr, event) {
+    const msgArr = [];
+    for (let i=0; i<nameArr.length; i++) {
+        msgArr.push('Thank you, ' + nameArr[i] + ', for the wonderful ' + event + ' gift!');
+    }
+    return msgArr;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}   
+//console.log(countDown(10));
